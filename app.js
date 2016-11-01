b3f01ef306719a05df6a564ed89de6c5b0c0c608
ZzzAoo <12826209@qq.com>
2016-11-01 18:01:50 +0800

    1，端口 2，icon

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.set("view options",{
     "close":"}}"
 });
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', '/images/icon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -87,5 +87,5 @@ app.use(function(err, req, res, next) {
 });
 
 
-//app.listen(3000);
+app.listen(3000);
 module.exports = app;
